2bbeaa863c21184acd6518deae098804aec04f54
nakedLLG <luchian_o_90@yahoo.com>
2017-04-25 18:56:24 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/', function(req,res){
+	res.send('hello');
+});
+app.get('/books', function(req,res){
+	res.send('hello books');
+});
+app.listen(5000, function(err){
+	console.log('running server ' + port);
+});
